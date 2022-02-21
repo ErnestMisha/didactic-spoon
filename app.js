cae1d726fdac7e2f875c67bb7bfc096c16f1cfce
Ernest Misha <pmiel13@gmail.com>
2022-02-21 19:16:07 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+
+import { router } from './routes.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use('/', router);
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).end();
+});
---
+++ routes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import axios from 'axios';
+import createError from 'http-errors';
+
+export const router = express.Router();
+
+router.get('/films', async (req, res, next) => {
+    let films;
+    try {
+        films = await axios.get('https://swapi.dev/api/films');
+        if(!films) {
+            next(createError(404));
+        }
+    }
+    catch(err) {
+        next(createError(500));
+    }
+    const list = [];
+    let id = 0;
+    films.data.results.forEach(film => {
+        list.push({
+            release_date: film.release_date,
+            title: film.title,
+            id: ++id
+        });
+    });
+    res.send({ films: list });
+});
